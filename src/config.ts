1167337ba228b523c3df72769964a35a5dde45df
Matin <matin2000haj@gmail.com>
2026-09-20 02:40:51 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -13,22 +13,22 @@ export const BASE = "/blog";
 // MORE SETTINGS:
 
 // will be used for the the title and meta tags and in the header (if SITE_NAME is left blank)
-export const SITE_TITLE = "flo-bit's blog template";
+export const SITE_TITLE = "Matin's blog";
 
 // will be used in the meta tags (and for example shown in search results)
-export const SITE_DESCRIPTION = "Welcome to my blog template! Fork it and make it your own.";
+export const SITE_DESCRIPTION = "Welcome to my blog website! I'll be writing some stuff here.";
 
 // will be used as the icon in the header and the favicon
 export const SITE_FAVICON = "🙃";
 
 // will be used in the footer as the name of the author (c) <YEAR> <NAME> - LICENSE
-export const NAME = "flo-bit";
+export const NAME = "Matin";
 
 // will be used in the footer as the license of the content (e.g. "All right reserved" or "CC-BY-SA 4.0")
 export const LICENSE = "MIT licensed.";
 
 // will be used to identify your bluesky account, so that likes and comments can be shown on your posts
-export const BLUESKY_IDENTIFIER = "flo-bit.dev";
+export const BLUESKY_IDENTIFIER = "bunnymat";
 
 export const SOURCE_LINK = 'https://github.com/flo-bit/blog-template';
 
@@ -52,9 +52,9 @@ export const SOCIAL_LINKS: {
   SHOW_RSS?: boolean;
 } = {
   SHOW_RSS: true,
-  BLUESKY_URL: "https://bsky.app/profile/flo-bit.dev",
-  GITHUB_URL: "https://github.com/flo-bit",
-  EMAIL: "flo.bit.dev@gmail.com",
+  BLUESKY_URL: "https://bsky.app/profile/bunnymat",
+  GITHUB_URL: "https://github.com/bunnymat",
+  EMAIL: "bunnymat@atomicmail.io",
 };
 
 
